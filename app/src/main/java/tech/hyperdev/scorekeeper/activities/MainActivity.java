a4ae03a4edcdd9077726d0d347c36a8cc3e0957f
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2017-07-18 02:22:11 +0800

    (4) Implement score counters.

--- app/src/main/java/tech/hyperdev/scorekeeper/activities/MainActivity.java
+++ app/src/main/java/tech/hyperdev/scorekeeper/activities/MainActivity.java
@@ -8,18 +8,56 @@ import tech.hyperdev.scorekeeper.fragments.ScoreFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ScoreFragment mTeamOneFragment;
+    private ScoreFragment mTeamTwoFragment;
+
+    private ScoreFragment.OnCounterIncrementListener mTeamOneIncrementListener =
+            new ScoreFragment.OnCounterIncrementListener() {
+                @Override
+                public void increment() {
+                    mTeamOneFragment.incrementCounter();
+                }
+            };
+    private ScoreFragment.OnCounterDecrementListener mTeamOneDecrementListener =
+            new ScoreFragment.OnCounterDecrementListener() {
+                @Override
+                public void decrement() {
+                    mTeamOneFragment.decrementCounter();
+                }
+            };
+    private ScoreFragment.OnCounterIncrementListener mTeamTwoIncrementListener =
+            new ScoreFragment.OnCounterIncrementListener() {
+                @Override
+                public void increment() {
+                    mTeamTwoFragment.incrementCounter();
+                }
+            };
+    private ScoreFragment.OnCounterDecrementListener mTeamTwoDecrementListener =
+            new ScoreFragment.OnCounterDecrementListener() {
+                @Override
+                public void decrement() {
+                    mTeamTwoFragment.decrementCounter();
+                }
+            };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ScoreFragment teamOneFragment = ScoreFragment.newInstance(getString(R.string.text_team_one));
-        ScoreFragment teamTwoFragment = ScoreFragment.newInstance(getString(R.string.text_team_two));
+        mTeamOneFragment = ScoreFragment.newInstance(getString(R.string.text_team_one));
+        mTeamTwoFragment = ScoreFragment.newInstance(getString(R.string.text_team_two));
+
+        mTeamOneFragment.setOnCounterIncrementListener(mTeamOneIncrementListener);
+        mTeamOneFragment.setOnCounterDecrementListener(mTeamOneDecrementListener);
+
+        mTeamTwoFragment.setOnCounterIncrementListener(mTeamTwoIncrementListener);
+        mTeamTwoFragment.setOnCounterDecrementListener(mTeamTwoDecrementListener);
 
         getSupportFragmentManager()
                 .beginTransaction()
-                .add(R.id.teamOneContainer, teamOneFragment)
-                .add(R.id.teamTwoContainer, teamTwoFragment)
+                .add(R.id.teamOneContainer, mTeamOneFragment)
+                .add(R.id.teamTwoContainer, mTeamTwoFragment)
                 .commit();
     }
 }
--- app/src/main/java/tech/hyperdev/scorekeeper/fragments/ScoreFragment.java
+++ app/src/main/java/tech/hyperdev/scorekeeper/fragments/ScoreFragment.java
@@ -10,11 +10,34 @@ import android.widget.TextView;
 
 import tech.hyperdev.scorekeeper.R;
 
-public class ScoreFragment extends Fragment {
+public class ScoreFragment extends Fragment implements View.OnClickListener {
+
+    /**
+     * The interface contract to use when incrementing score counter.
+     */
+    public interface OnCounterIncrementListener {
+        void increment();
+    }
+
+    /**
+     * The interface contract to use when decrementing score counter.
+     */
+    public interface OnCounterDecrementListener {
+        void decrement();
+    }
 
     private static final String KEY_TEAM_NAME = "key-team-name";
-    private TextView mTvTeamName;
 
+    private TextView mTvTeamName;
+    private TextView mTvCounter;
+
+    private int mCounter = 0;
+    private OnCounterIncrementListener mCounterIncrementListener;
+    private OnCounterDecrementListener mCounterDecrementListener;
+
+    /**
+     * Creates a new instance of {@link ScoreFragment} with passed team name.
+     */
     public static ScoreFragment newInstance(String teamName) {
         Bundle args = new Bundle();
         args.putString(KEY_TEAM_NAME, teamName);
@@ -22,6 +45,34 @@ public class ScoreFragment extends Fragment {
         ScoreFragment fragment = new ScoreFragment();
         fragment.setArguments(args);
         return fragment;
+    }
+
+    /**
+     * Gets the counter's increment listener.
+     */
+    public OnCounterIncrementListener getOnCounterIncrementListener() {
+        return mCounterIncrementListener;
+    }
+
+    /**
+     * Sets the counter's increment listener.
+     */
+    public void setOnCounterIncrementListener(OnCounterIncrementListener counterIncrementListener) {
+        mCounterIncrementListener = counterIncrementListener;
+    }
+
+    /**
+     * Gets the counter's decrement listener.
+     */
+    public OnCounterDecrementListener getOnCounterDecrementListener() {
+        return mCounterDecrementListener;
+    }
+
+    /**
+     * Sets the counter's decrement listener.
+     */
+    public void setOnCounterDecrementListener(OnCounterDecrementListener counterDecrementListener) {
+        mCounterDecrementListener = counterDecrementListener;
     }
 
     @Override
@@ -40,5 +91,43 @@ public class ScoreFragment extends Fragment {
 
     private void findViews(View view) {
         mTvTeamName = (TextView) view.findViewById(R.id.tvTeamName);
+        mTvCounter = (TextView) view.findViewById(R.id.tvCounter);
+
+        view.findViewById(R.id.btnPlus).setOnClickListener(this);
+        view.findViewById(R.id.btnMinus).setOnClickListener(this);
+    }
+
+    /**
+     * Increments counter value and updates the result to the view.
+     */
+    public void incrementCounter() {
+        mCounter++;
+        mTvCounter.setText(String.valueOf(mCounter));
+    }
+
+    /**
+     * Decrements counter value and updates the result to the view.
+     */
+    public void decrementCounter() {
+        mCounter--;
+        mTvCounter.setText(String.valueOf(mCounter));
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.btnPlus:
+                if (mCounterIncrementListener != null) {
+                    mCounterIncrementListener.increment();
+                }
+                break;
+
+            case R.id.btnMinus:
+                if (mCounterDecrementListener != null) {
+                    mCounterDecrementListener.decrement();
+                }
+                break;
+        }
     }
 }
