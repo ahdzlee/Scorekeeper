5730e8ca354df42e9e1f147350e3af4f33ec44ed
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2017-07-18 03:39:36 +0800

    (6) Implement day and night theme modes.

--- app/src/main/java/tech/hyperdev/scorekeeper/activities/MainActivity.java
+++ app/src/main/java/tech/hyperdev/scorekeeper/activities/MainActivity.java
@@ -2,6 +2,9 @@ package tech.hyperdev.scorekeeper.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import tech.hyperdev.scorekeeper.R;
 import tech.hyperdev.scorekeeper.fragments.ScoreFragment;
@@ -80,5 +83,41 @@ public class MainActivity extends AppCompatActivity {
         // Save the fragments' instance state
         getSupportFragmentManager().putFragment(outState, KEY_FRAGMENT_TEAM_ONE, mTeamOneFragment);
         getSupportFragmentManager().putFragment(outState, KEY_FRAGMENT_TEAM_TWO, mTeamTwoFragment);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu from XML
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        // Change the label of the menu based on the state of the app
+        int nightMode = AppCompatDelegate.getDefaultNightMode();
+        if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+            menu.findItem(R.id.menu_night_mode).setTitle(R.string.menu_day_mode);
+        } else {
+            menu.findItem(R.id.menu_night_mode).setTitle(R.string.menu_night_mode);
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Check if correct item was clicked
+        if (item.getItemId() == R.id.menu_night_mode) {
+            // Get night mode state of the app
+            int nightMode = AppCompatDelegate.getDefaultNightMode();
+
+            // Set the theme mode for the restarted activity
+            if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            }
+        }
+
+        recreate();
+
+        return true;
     }
 }
